f58d0c38950f40cbd31d2fc6a469b376f4306683
Makson <maksonsantoss4@gmail.com>
2023-01-03 12:19:49 -0300

    feat: inicio do projeto com model user herdada do AbstractUser

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    age = models.PositiveSmallIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
