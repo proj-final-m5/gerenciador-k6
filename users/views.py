b1d97d2ab7725245f9e017cee163e6e7fde4085e
CarolPezzin <102972588+CarolPezzin@users.noreply.github.com>
2023-01-03 16:09:44 -0300

    login finalizado, fiz o usuario para poder testar o login, mas apenas a criação de user

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from users.models import User
+from users.serializers import UserSerializer
+from .permissions import IsAccountOwner
 
-# Create your views here.
+class UserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAccountOwner]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
