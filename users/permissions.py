8eb32f5b5e95137d799ef4d02eaa15e7dbdc9f5b
Makson <maksonsantoss4@gmail.com>
2023-01-04 13:38:28 -0300

    fix: Correções login e rotas de usuário

--- users/permissions.py
+++ users/permissions.py
@@ -5,4 +5,4 @@ from rest_framework.views import View
 
 class IsAccountOwner(permissions.BasePermission):
     def has_object_permission(self, request, view: View, obj: User) -> bool:
-        return request.user.is_authenticated and obj == request.user
+        return request.user.is_authenticated and obj.id == request.user.id
