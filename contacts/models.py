9f04e0515e2314aa24fa5e014f7b2330cff4787b
Leandro Schillreff <leandroschillreff@gmail.com>
2023-01-04 11:46:29 -0400

    feat: criação do modelo de contato

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    class Meta:
+        ordering = ["id"]
+
+    name = models.CharField(max_length=127)
+    email = models.EmailField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="contacts",)
