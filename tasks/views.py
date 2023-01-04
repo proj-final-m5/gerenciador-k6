1391ce2da3751bdedbe71a5c1a83551761f6a768
= <r.netofreire45@gmail.com>
2023-01-04 13:13:12 -0300

    feat/tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from .models import Task
 from .serializers import TaskSerializer
+from django.shortcuts import get_object_or_404
 
 class TaskView(CreateAPIView,ListAPIView):
   authentication_classes = [JWTAuthentication]
   queryset = Task.objects.all()
+  serializer_class = TaskSerializer
+  def perform_create(self, serializer):
+        categories = get_object_or_404(Task, self.request.body.categories)
+        serializer.save(user=self.request.user,categories=categories)
+
+class TaskDetailView(RetrieveUpdateDestroyAPIView):
+  authentication_classes = [JWTAuthentication]
+  queryset= Task.objects.all()
   serializer_class = TaskSerializer
